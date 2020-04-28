c3e367f4a69fac541a1ca54f47cde8329682d02e
Timaska20 <tamirlansuska@gmail.com>
2020-04-28 23:23:53 +0600

    initializing repository

---
+++ VKstik.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import os
+while True:
+  case=str(input("for PC 1\nfor android 2 \n"))
+  if case== "1":
+	  p="\\"
+	  break
+  elif case =="2":
+	  p="/"
+	  break
+  else:
+  	continue 
+	  
+	
+token = "a9fc96e88ab9176a79d899440c74b2f5df41f55980d7cbcd691635c75a18bc0f7b41d0f3d4893f10c1ea4"
+peerID="1"
+version="5.103"
+filename = str(input('Ведите название файла без .png:\n'))
+def getMessagesUploadServer():
+    response = requests.get('https://api.vk.com/method/docs.getMessagesUploadServer',params={'access_token':token,
+                                                                                             'v':version,
+                                                                                             'type':"graffiti",
+                                                                                             "peer_id":peerID}).json()
+    return response['response']['upload_url']
+def save(file):
+    response2 = requests.get('https://api.vk.com/method/docs.save', params={'access_token': token,
+                                                                                              'v': version,
+                                                                                              'file':file}).json()
+    print(response2)
+
+def main():
+  uploadurl = getMessagesUploadServer()
+  result=requests.post(uploadurl, files={'file': open(os.getcwd()+p + filename + '.png', 'rb')}).json()
+  IDfile= result['file']
+  save(IDfile)
+if __name__ == '__main__':
+    main()
