d7572eeee936661e10dd4d5a202a0b2cde7005ba
Timaska20 <tamirlansuska@gmail.com>
2020-05-01 22:47:19 +0600

    added ending on graffiti

--- VKstik.py
+++ VKstik.py
@@ -13,10 +13,12 @@ while True:
   	continue 
 	  
 	
-token = "a9fc96e88ab9176a79d899440c74b2f5df41f55980d7cbcd691635c75a18b************a4"#токен 
+token = "f202659091a9d880d72b060d697e0d6842902c63aaac00d27344860db67454*****************0db3e6"
 peerID="1"
 version="5.103"
 filename = str(input('Ведите название файла без .png:\n'))
+filename_grafity=str(filename+"_grafity")
+os.rename(filename+".png",filename_grafity+".png")
 def getMessagesUploadServer():
     response = requests.get('https://api.vk.com/method/docs.getMessagesUploadServer',params={'access_token':token,
                                                                                              'v':version,
@@ -26,12 +28,13 @@ def getMessagesUploadServer():
 def save(file):
     response2 = requests.get('https://api.vk.com/method/docs.save', params={'access_token': token,
                                                                                               'v': version,
-                                                                                              'file':file}).json()
+                                                                                              'file':file
+                                                                                              }).json()
     print(response2)
 
 def main():
   uploadurl = getMessagesUploadServer()
-  result=requests.post(uploadurl, files={'file': open(os.getcwd()+p + filename + '.png', 'rb')}).json()
+  result=requests.post(uploadurl, files={'file': open(os.getcwd()+p + filename_grafity + '.png', 'rb')}).json()
   IDfile= result['file']
   save(IDfile)
 if __name__ == '__main__':
